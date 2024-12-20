a911cabfa3cf8b3e6f1429f5e2ff94074d1ad181
yubkek <fluenm@gmail.com>
2024-12-20 11:24:50 +1100

    added image assets and buttons started

--- main.py
+++ main.py
@@ -4,53 +4,42 @@ from widgets import *
 W = 1200
 H = 900
 
-class Paint:
+class Paint(tk.Tk):
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title('Paint')
-        self.window.geometry(f'{W}x{H}+700+300')
-        self.window.rowconfigure((0,1,2,3,4,5), weight=1, uniform='a')
-        self.window.columnconfigure((0,1,2), weight=1, uniform='a')
+        # paint variables
+        current_color = 'black'
+        current_brush_size = 3
 
+        # window set up
+        super().__init__()
+        self.title('Paint')
+        self.geometry(f'{W}x{H}+700+300')
+        self.rowconfigure((0,1,2,3,4,5), weight=1, uniform='a')
+        self.columnconfigure((0,1,2), weight=1, uniform='a')
+        self.resizable(width=False, height=False)
+
+        # create widgets 
         self.create_widgets()
         self.place_widgets()
 
-    def run(self):
-        self.window.mainloop()
-
+        # run
+        self.mainloop()
+        
     def create_widgets(self):
         # frames
-        self.canvas_frame = Frame(self.window)
-        self.button_frame = Frame(self.window)
-        self.slider_frame = Frame(self.window)
-
-        # canvas
-        self.canvas = Canvas(self.canvas_frame.frame)
-
-        # buttons
-        self.red_button = Button(self.button_frame.frame)
-        self.blue_button = Button(self.button_frame.frame)
-        self.green_button = Button(self.button_frame.frame)
-        self.orange_button = Button(self.button_frame.frame)
-        self.yellow_button = Button(self.button_frame.frame)
-        self.eraser_button = Button(self.button_frame.frame)
-        self.fill_button = Button(self.button_frame.frame)
-        self.clear_button = Button(self.button_frame.frame)
-
-        # slider
-        self.thickness_slider = Slider(self.slider_frame.frame)
+        self.canvas_frame = CanvasFrame(self)
+        self.button_frame = ButtonFrame(self)
+        self.slider_frame = SliderFrame(self)
 
     def place_widgets(self):
         # canvas frame
-        self.canvas_frame.frame.grid(row=0, column=0, rowspan=5, columnspan=2, sticky='nsew')
-        self.canvas.canvas.pack(expand=True, fill='both')
+        self.canvas_frame.grid(row=0, column=0, rowspan=5, columnspan=2, sticky='nsew')
 
         # button frame
-        
+        self.button_frame.grid(row=0, column=2, rowspan=5, columnspan=1, sticky='nsew')
 
         # slider frame
-
+        self.slider_frame.grid(row=5, column=0, columnspan=2, sticky='nsew')
 
 if __name__ == "__main__":
     paint_app = Paint()
-    paint_app.run()
--- widgets.py
+++ widgets.py
@@ -1,18 +1,53 @@
 from tkinter import ttk
 import tkinter as tk
 
-class Canvas:
+class ButtonFrame(tk.Frame):
     def __init__(self, window):
-        self.canvas = tk.Canvas(window, bg='white')
+        super().__init__(window)
+    
+        # frame set up
+        self.rowconfigure((0,1,2,3,4,5), weight=1, uniform='a')
+        self.columnconfigure((0,1), weight=1, uniform='a')
 
-class Button:
-    def __init__(self, window):
-        self.button = ttk.Button(window)
+        # create frame widgets
+        self.create_widgets()
 
-class Slider:
-    def __init__(self, window):
-        self.slider = ttk.Scale(window)
+    def create_widgets(self):
+        # color buttons
+        self.black_button = tk.Button(self, background='black')
+        self.red_button = tk.Button(self, background='red')
+        self.blue_button = tk.Button(self, background='blue')
+        self.green_button = tk.Button(self, background='green')
+        self.yellow_button = tk.Button(self, background='yellow')
 
-class Frame:
+        # tool buttons
+        self.eraser_button = ttk.Button(self)
+        self.clear_button = ttk.Button(self)
+        self.fill_button = ttk.Button(self)
+
+        # place
+        self.black_button.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
+        self.red_button.grid(row=4, column=0, sticky='nsew', padx=10, pady=10)
+        self.blue_button.grid(row=1, column=0, sticky='nsew', padx=10, pady=10)
+        self.green_button.grid(row=3, column=0, sticky='nsew', padx=10, pady=10)
+        self.yellow_button.grid(row=2, column=0, sticky='nsew', padx=10, pady=10)
+        self.eraser_button.grid(row=0, column=1, sticky='nsew', padx=10, pady=10)
+        self.clear_button.grid(row=2, column=1, sticky='nsew', padx=10, pady=10)
+        self.fill_button.grid(row=1, column=1, sticky='nsew', padx=10, pady=10)
+
+
+class CanvasFrame(tk.Frame):
     def __init__(self, window):
-        self.frame = tk.Frame(window, bg="red")
+        super().__init__(window, bg='white')
+        
+        # create frame widgets
+        self.create_widgets()
+
+    def create_widgets(self):
+        # main canvas
+        self.canvas = tk.Canvas(self, background='red')
+        self.canvas.pack(expand=True, fill='both')
+
+class SliderFrame(tk.Frame):
+    def __init__(self, window):
+        super().__init__(window, bg='white')
